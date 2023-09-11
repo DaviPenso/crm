6a29db50b79a1a5b4b1716054c689c230ed5d1dd
Davi Penso <davi.penso@premye.com.br>
2023-09-10 22:38:34 -0300

    feat:Envio de email com requisição ajax utilizando jQuery

---
+++ actions/contato.php
@@ -0,0 +1,70 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $destinatario = $_POST["destinatario"];
+    $assunto = $_POST["nome"];
+    $mensagem = $_POST["mensagem"];
+
+    // Configurar cabeçalhos para o email
+
+    $headers = "From: davipenso@gmail.com" . "\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+
+    // Enviar o email
+
+/*    if (mail($destinatario, $nome, $mensagem, $headers)) {
+        echo "Email enviado com sucesso!";
+    } else {
+        echo "Falha ao enviar o email.";
+    } */
+}
+
+/* Envio do Email */
+include './vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true); // Instancia a classe e cria um objeto
+
+try {
+    //Server settings
+    $mail->isSMTP();
+    $mail->SMTPDebug  = 4;
+    $mail->Host       = 'smtp.hostinger.com';
+    $mail->Port       = 587;
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'noreply@sabercristao.com.br';
+    $mail->Password   = 'Noreply159753@!';
+    
+    //Recipients
+    $mail->setFrom("noreply@sabercristao.com.br", "Saber Cristão | Não responda!!");
+    $mail->addAddress("felipefcamp@gmail.com", "Felipe Campelo");
+    $mail->addCC('felipefcampdev@gmail.com');
+
+    //Content
+    $dataHoje = date("d/m/Y") . " às " . date("H:i:s");
+
+    $mail->isHTML(true);
+    $mail->Subject = "Mensagem de Cliente em CRM";
+    $mail->CharSet = 'UTF-8';
+    $mail->Body    = "
+        <p>Nome: {$nome}</p>
+        <p>Mensagem: {$mensagem}</p>
+
+        <br><br>
+
+        Email enviado em {$dataHoje}
+    ";
+
+    $mail->send();
+    echo 'Mensagem enviada com sucesso!';
+} catch (Exception $e) {
+    echo "A mensagem não pôde ser enviada. Erro: {$mail->ErrorInfo}";
+}
+
+
+
+
+?>
--- contato.php
+++ contato.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
@@ -16,10 +17,10 @@
     </div>
     <div class="container mt-5">
         <main>
-            <form id="contato">
+            <form id="contato_clientes">
                 <div class="form-group">
                     <label for="nome">Nome</label>
-                    <input type="text" class="form-control" id="nome" placeholder="Nome">
+                    <input type="text" class="form-control" id="nome" placeholder="Nome Completo">
                 </div>
                 <div class="form-group mt-3">
                     <label for="mensagem">Mensagem</label>
@@ -29,33 +30,34 @@
             </form>
 
             <div id="mensagem-resposta"></div>
+            <div id="destinatario"></div>
 
-<script>
-    $(document).ready(function() {
-        $('#email-form').submit(function(e) {
-            e.preventDefault(); // Evita o envio padrão do formulário
+            <script>
+               // $(document).ready(function() {
+               //     $('#contato_clientes').submit(function(e) {
+               //         e.preventDefault(); // Evita o envio padrão do formulário
 
-            // Coleta os dados do formulário
-            var destinatario = $('#destinatario').val();
-            var assunto = $('#assunto').val();
-            var mensagem = $('#mensagem').val();
+                        // Coleta os dados do formulário
+                        var destinatario = "davipenso@gmailcom";
+                        var nome = $('#nome').val();
+                        var mensagem = $('#mensagem').val();
 
-            // Envia os dados para o servidor usando AJAX
-            $.ajax({
-                url: 'enviar_email.php',
-                type: 'POST',
-                data: {
-                    destinatario: destinatario,
-                    assunto: assunto,
-                    mensagem: mensagem
-                },
-                success: function(response) {
-                    $('#mensagem-resposta').html(response);
-                }
-            });
-        });
-    });
-</script>
+                        // Envia os dados para o servidor usando AJAX
+                        $.ajax({
+                            url: './actions/contato.php',
+                            type: 'POST',
+                            data: {
+                                destinatario: destinatario,
+                                nome: nome,
+                                mensagem: mensagem
+                            },
+                            success: function(response) {
+                                $('#mensagem-resposta').html(response);
+                            }
+                        });
+                //    });
+            //    });
+            </script>
 </body>
 </html>
         </main>
@@ -63,6 +65,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    
     <script src="./scripts.js"></script>
 </body>
 
